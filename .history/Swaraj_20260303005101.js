82f965ee2a8a5ace37b93813722a32bea3513953
Swaraj Bhumare <swarajbhumare2021@gmail.com>
2026-03-03 00:51:48 +0530

    small c

---
+++ .history/Swaraj_20260303005101.js
@@ -0,0 +1,7 @@
+console.log("Hello, Swaraj!");
+console.log("Hello, Swarajya!");
+swaraj = "Swarajya";
+console.log("Hello, Swaraj and Swarajya!");
+Swaraj = "Swaraj";
+Swarajya + "Swaraj" + main;
+Swaraj + "Swarajya" + Main;
